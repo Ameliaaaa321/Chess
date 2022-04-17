94f3c059b8487c4156dd7ac817fea7623c036931
Ameliaaaa321 <103305561+Ameliaaaa321@users.noreply.github.com>
2022-04-17 21:38:56 +0800

    Create Board.java

---
+++ src/com/company/Board.java
@@ -0,0 +1,14 @@
+
+// 储存棋盘上的所有格子
+public class Board {
+    Position[][] positions = new Position[9][9];
+
+    public Board() {
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= 8; j++) {
+                Position p = new Position(i, j);
+                positions[i][j] = p;
+            }
+        }
+    }
+}
